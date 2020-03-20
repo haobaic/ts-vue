9d6be3f846c29bf4806bf84d3cca86b7cf97c87b
haobaic <1849174241@qq.com>
2020-03-20 19:08:22 +0800

    table

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -2,3 +2,5 @@
 const login="/api/users/login";
 //修改密码
 const findPwd="/api/users/findPwd";
+//修改账号密码
+const changePwd="/api/users/changePwd";
--- src/views/DataManage/TableData.vue
+++ src/views/DataManage/TableData.vue
@@ -5,11 +5,16 @@
   </template>
   
   <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
+  import { Component, Vue, Provide } from 'vue-property-decorator';
   
   @Component
   export default class TableData extends Vue {
-    
+    @Provide() searchVal:String="";
+    @Provide() tHeight:number=document.body.offsetHeight-270;
+    @Provide() tableData:any=[]; //表格数据
+    @Provide() page:Number=1; //当前page
+    @Provide() size:Number=5;//请求数据的个数 默认5
+    @Provide() total:Number=0;//总条数
   }
   </script>
   <style scoped lang="scss">
